82d4453c9626f7c31b8cfbce76e289e0c303128b
Aranthiël <nmoronidalmasso@gmail.com>
2022-08-02 00:51:37 -0300

    módilos banner y logo Ap cargados

---
+++ src/app/components/headder/headder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headder',
+  templateUrl: './headder.component.html',
+  styleUrls: ['./headder.component.css']
+})
+export class HeadderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
